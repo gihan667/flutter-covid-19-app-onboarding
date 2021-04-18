781fd7b0b95dcb2038f28f02c16ad5812924a8a3
Gihan Lakshan <gihan@proitsolutions.lk>
2021-04-18 17:39:11 +0530

    initial commit

---
+++ lib/Screens/onboarding/components/skip_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_onboarding/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SkipButton extends StatelessWidget {
+  const SkipButton({
+    Key key,
+    @required this.tapEvent
+  }) : super(key: key);
+
+  final GestureTapCallback tapEvent;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return InkWell(
+      onTap: tapEvent,
+      child: Container(
+        width: (150 / 414) * size.width,
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Skip',
+              style: TextStyle(
+                color: kTextLightColor,
+                fontSize: 16
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
